5d1f878c9a300aa749aab992a9c001a75a9d5261
rutujadabhade04 <168404028+rutujadabhade04@users.noreply.github.com>
2025-12-06 14:48:13 +0530

    Update WebSocket URL

--- src/App.jsx
+++ src/App.jsx
@@ -116,7 +116,7 @@ function App() {
   // main use effect for communication with server
   useEffect(() => {
     // newSocket.current = new WebSocket("ws://localhost:2121");
-     newSocket.current = new WebSocket("https://f3aa6a7225ab.ngrok-free.app/");
+     newSocket.current = new WebSocket("https://95cf0dd82593.ngrok-free.app/");
 
     newSocket.current.onopen = () => {
       setSocketMessage("Connected!");
